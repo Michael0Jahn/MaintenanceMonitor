8f6e667bce700015b4a30272b25543b3b1e75b87
Michael0Jahn <ic21b029@technikum-wien.at>
2022-11-28 20:46:28 +0100

    added defaultMessage and serviceMessage Methods

--- src/main/java/at/fhtw/bic/maintenancemonitor/controller/MaintenanceController.java
+++ src/main/java/at/fhtw/bic/maintenancemonitor/controller/MaintenanceController.java
@@ -6,11 +6,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 
 public class MaintenanceController {
+    private static String message = "";
 
-    @GetMapping("/hi")
-    public int maintMessage(){
-        int i = 5;
-        return i;
+    @GetMapping()
+    public String Message(){
+        return message;
+    }
+    @GetMapping("/status/default")
+    public String defaultMessage(){
+        message = "Everything operates as expected";
+        return message;
     }
 
+    @GetMapping("/status")
+    public String serviceMessage(String newMessage){
+        message = newMessage;
+        return message;
+    }
 }
